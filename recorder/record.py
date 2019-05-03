9cc5ea60eb2a7eedf48ce50ca0e28666d2d1adc5
Ty Schenk <tyschenk2@me.com>
2019-05-03 15:45:43 -0400

    enhancements

--- recorder/record.py
+++ recorder/record.py
@@ -7,6 +7,7 @@ import json
 import pycurl
 import sys
 import time
+import datetime
 import signal
 import os.path
 import argparse
@@ -14,11 +15,14 @@ import argparse
 parser = argparse.ArgumentParser(description="record all blocks on Nano network")
 parser.add_argument('-nu', '--node_url', type=str, help='Nano node url', default='127.0.0.1')
 parser.add_argument('-np', '--node_port', type=int, help='Nano node port', default=55000)
+parser.add_argument('-sn', '--save_num', type=int, help='Save blocks to disk how often', default=60)
 options = parser.parse_args()
+SAVE_EVERY_N = options.save_num
 
 # add a circuit breaker variable
 global signaled
 signaled = False
+lastSave = datetime.datetime.now()
 
 def communicateNode(rpc_command):
     buffer = BytesIO()
@@ -69,6 +73,7 @@ def writeJson(filename, data):
 
 # execute recording responsibilities
 def start():
+	global lastSave
 	# notify system that the recording has started
 	print('Recorder started')
 
@@ -85,6 +90,7 @@ def start():
 
 	# record blocks continuously
 	while True:
+		start_time = time.time()
 		confirmations = getConfirmations()['confirmations']
 		newBlocks = getBlocks()
 
@@ -100,13 +106,17 @@ def start():
 		blocks['times'][currentTime] = {"time": currentTime, "checked": newBlocks['count'], "unchecked": newBlocks['unchecked']}
 
 		# save changes
-		writeJson('data.json', data)
-		writeJson('blockcounts.json', blocks)
+		currentTime = datetime.datetime.now()
+		timeDiff = currentTime - lastSave
+		if SAVE_EVERY_N <= timeDiff.seconds:
+			writeJson('data.json', data)
+			writeJson('blockcounts.json', blocks)
+			lastSave = currentTime
+			# notify system when the data was last saved
+			print ('saved data at: ' + time.strftime("%I:%M:%S"))
 
-		# notify system when the data was last saved
-		print ('saved data at: ' + time.strftime("%I:%M:%S"))
-
-		# sleep for 0.1 seconds
-		time.sleep(0.1)
+		print("Recorded Blocks. Execution time: %s seconds" % (time.time() - start_time))
+		# sleep for 0.01 seconds
+		time.sleep(0.01)
 
 start()
